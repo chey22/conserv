259d6ec0493046f3c5901b26e59d6dc088cf0b87
chey22 <cheyennemangum@gmail.com>
2021-05-08 00:15:08 -0500

    dunzo

--- src/Components/Charts/Temp.tsx
+++ src/Components/Charts/Temp.tsx
@@ -18,7 +18,6 @@ const CustomTooltip: FC = (props: any) => {
 
   if (active) {
     const data = payload[0].payload;
-    console.log(data)
     return (
       <Card style={{ width: "auto" }}>
         <Card.Header>
